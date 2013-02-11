5688ee3a2a3afa87fd49f1fc31fbbc1efb4ee6a1
Jan Michelfeit <michelfeit.jan@gmail.com>
2013-02-12 00:04:22 +0100

    created maven project

---
+++ src/main/java/cz/cuni/mff/odcleanstore/crbatch/Application.java
@@ -0,0 +1,13 @@
+package cz.cuni.mff.odcleanstore.crbatch;
+
+public final class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("It works!");
+	}
+
+}
