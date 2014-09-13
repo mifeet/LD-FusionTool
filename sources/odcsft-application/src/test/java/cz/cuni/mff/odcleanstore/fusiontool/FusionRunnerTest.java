70bae984910980b3ef7a5f39225421c45abf4853
Jan Michelfeit <jmichelf@cisco.com>
2014-09-14 01:17:49 +0200

    FusionToolRunner test

--- sources/odcsft-application/src/test/java/cz/cuni/mff/odcleanstore/fusiontool/FusionRunnerTest.java
+++ sources/odcsft-application/src/test/java/cz/cuni/mff/odcleanstore/fusiontool/FusionRunnerTest.java
@@ -3,12 +3,19 @@ package cz.cuni.mff.odcleanstore.fusiontool;
 import cz.cuni.mff.odcleanstore.fusiontool.conflictresolution.ResourceDescriptionConflictResolver;
 import cz.cuni.mff.odcleanstore.fusiontool.conflictresolution.urimapping.UriMappingIterable;
 import cz.cuni.mff.odcleanstore.fusiontool.loaders.InputLoader;
+import cz.cuni.mff.odcleanstore.fusiontool.util.EnumFusionCounters;
+import cz.cuni.mff.odcleanstore.fusiontool.util.ProfilingTimeCounter;
 import cz.cuni.mff.odcleanstore.fusiontool.writers.CloseableRDFWriter;
 import cz.cuni.mff.odcleanstore.fusiontool.writers.UriMappingWriter;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.openrdf.model.Model;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.mockito.Mockito.*;
 
 public class FusionRunnerTest {
@@ -26,11 +33,18 @@ public class FusionRunnerTest {
     @Before
     public void setUp() throws Exception {
         componentFactory = mock(FusionComponentFactory.class);
+
         uriMapping = mock(UriMappingIterable.class);
         when(componentFactory.getUriMapping()).thenReturn(uriMapping);
 
         metadata = mock(Model.class);
-        when(componentFactory.getMetadata()).thenReturn(metadata);
+        when(componentFactory.getMetadata()).thenAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Thread.sleep(10);
+                return metadata;
+            }
+        });
 
         inputLoader = mock(InputLoader.class);
         when(componentFactory.getInputLoader()).thenReturn(inputLoader);
@@ -63,4 +77,23 @@ public class FusionRunnerTest {
         verify(sameAsWriter).write(uriMapping);
     }
 
+    @Test
+    public void measuresTimeWhenProfilingIsOn() throws Exception {
+        FusionRunner runner = new FusionRunner(componentFactory);
+        runner.setProfilingOn(true);
+        runner.runFusionTool();
+
+        ProfilingTimeCounter<EnumFusionCounters> profiler = runner.getTimeProfiler();
+        assertThat(profiler.getCounter(EnumFusionCounters.META_INITIALIZATION), greaterThan(0L));
+    }
+
+    @Test
+    public void doesNotMeasureTimeWhenProfilingIsOff() throws Exception {
+        FusionRunner runner = new FusionRunner(componentFactory);
+        runner.setProfilingOn(false);
+        runner.runFusionTool();
+
+        ProfilingTimeCounter<EnumFusionCounters> profiler = runner.getTimeProfiler();
+        assertThat(profiler.getCounter(EnumFusionCounters.META_INITIALIZATION), is(0L));
+    }
 }
